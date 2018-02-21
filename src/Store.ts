298c3402447a37f9e07957d7e8fcb5e02dbe4ae2
Fahad Hossain <evilinfinite7@gmail.com>
2018-02-21 15:41:54 +0600

    Add MST store

---
+++ src/Store.ts
@@ -0,0 +1,41 @@
+import { types } from 'mobx-state-tree';
+
+const random = (): string =>
+  Number(1 + Math.floor(Math.random() * 10000)).toString(24);
+
+const Contact = types
+  .model('Contact', {
+    name: types.optional(types.string, 'Unnamed Contact'),
+    telephones: types.optional(types.array(types.string), []),
+    address: types.optional(types.string, ''),
+    email: types.optional(types.string, '')
+  })
+  .views(self => ({
+    get phone() {
+      return self.telephones.entries();
+    }
+  }))
+  .actions(self => {
+    function setName(name: string) {
+      self.name = name;
+    }
+
+    function addPhone(phone: string) {
+      self.telephones.push(phone);
+    }
+
+    return { setName, addPhone };
+  });
+
+const AddressBook = types
+  .model('AddressBook', {
+    contacts: types.optional(types.map(Contact), {})
+  })
+  .actions(self => {
+    function addContact(contact: object) {
+      self.contacts.set(random(), Contact.create(contact));
+    }
+    return { addContact };
+  });
+
+export { AddressBook };
