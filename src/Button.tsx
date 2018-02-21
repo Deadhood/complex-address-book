e140c367cb3678ec3487448b7440f2420c7c5db2
Main Uddin <mdmain74@gmail.com>
2018-02-21 17:32:05 +0600

    added some modules

---
+++ src/Button.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+// import './App.css';
+import { Icon } from 'antd';
+
+export const Button = () => (
+  <div>
+    <Icon type="plus" />
+  </div>
+);
+// class component
+// class Button extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         hello from button
+//         <Icon type="plus-square" />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Button;
+
+// functional component
